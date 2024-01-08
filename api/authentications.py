b7b3e3e375192cfe6b32b99563df6092e9e1f653
gesrygsy <gesrygsy@yahoo.co.jp>
2024-01-08 21:52:12 +0800

    fix bug

--- api/authentications.py
+++ api/authentications.py
@@ -17,8 +17,8 @@ router = fastapi.APIRouter(
 def login(request: OAuth2PasswordRequestForm =Depends(), db: Session =Depends(get_db)):
     user = get_user_by_username(db=db, username=request.username)
     if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
-    if Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")    
+    if not Hash.verify(hashed_password=user.password, plain_password=request.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password.", headers={"WWW-Authenticate": "Bearer"})
     
     access_token = create_access_token(
